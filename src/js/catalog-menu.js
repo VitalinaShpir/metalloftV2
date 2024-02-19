78ec2a78a1ba1c5cab37872ee80f88da3ad9e88e
VitalinaShpir <grigorenko.vitalina@gmail.com>
2024-02-19 13:35:54 +0200

    add2menu

--- src/js/catalog-menu.js
+++ src/js/catalog-menu.js
@@ -1,6 +1,6 @@
-const list = document.querySelector('.catalog-item');
+const listTable = document.querySelector('.home-table');
 
-list.addEventListener('click', onOpenModal);
+listTable.addEventListener('click', onOpenModal);
 
 function onOpenModal() {
   const catalogMenu = document.querySelector('.catalog-menu');
@@ -8,6 +8,19 @@ function onOpenModal() {
 }
 
 // function onCloseModal() {
-//   window.removeEventListener('keydown', onEscKeyPress);
+//   window.addEventListener('keydown', onEscKeyPress);
 //   catalogMenu.classList.remove('catalog-open');
 // }
+
+const listStellage = document.querySelector('.home-stellage');
+
+listStellage.addEventListener('click', onOpenStellage);
+
+function onOpenStellage() {
+  const stellMenu = document.querySelector('.stellage-menu');
+
+  console.log('<<<===============>>>');
+  console.log(stellMenu);
+  console.log('<<<===============>>>');
+  stellMenu.classList.add('catalog-open');
+}
