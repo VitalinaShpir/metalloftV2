2fa90bdfecc4b61150f943b5f1970482af97039c
VitalinaShpir <grigorenko.vitalina@gmail.com>
2024-02-19 14:26:15 +0200

    add3menu

--- src/js/catalog-menu.js
+++ src/js/catalog-menu.js
@@ -1,26 +1,44 @@
 const listTable = document.querySelector('.home-table');
+const closeButton = document.querySelector('.close-table');
+const catalogMenu = document.querySelector('.catalog-menu');
+
+// for each????
 
 listTable.addEventListener('click', onOpenModal);
+closeButton.addEventListener('click', onCloseModal);
 
 function onOpenModal() {
-  const catalogMenu = document.querySelector('.catalog-menu');
   catalogMenu.classList.add('catalog-open');
 }
 
-// function onCloseModal() {
-//   window.addEventListener('keydown', onEscKeyPress);
-//   catalogMenu.classList.remove('catalog-open');
-// }
+function onCloseModal() {
+  catalogMenu.classList.remove('catalog-open');
+}
 
 const listStellage = document.querySelector('.home-stellage');
+const stellMenu = document.querySelector('.stellage-menu');
+const closeStellage = document.querySelector('.close-stellage');
 
 listStellage.addEventListener('click', onOpenStellage);
+closeStellage.addEventListener('click', onCloseStellage);
 
 function onOpenStellage() {
-  const stellMenu = document.querySelector('.stellage-menu');
-
-  console.log('<<<===============>>>');
-  console.log(stellMenu);
-  console.log('<<<===============>>>');
   stellMenu.classList.add('catalog-open');
+}
+function onCloseStellage() {
+  stellMenu.classList.remove('catalog-open');
+}
+
+const listInterier = document.querySelector('.home-interier');
+const interMenu = document.querySelector('.interier-menu');
+const closeInter = document.querySelector('.close-inter');
+
+listInterier.addEventListener('click', onOpenInterier);
+closeInter.addEventListener('click', onCloseInterier);
+
+function onOpenInterier() {
+  interMenu.classList.add('catalog-open');
+}
+function onCloseInterier() {
+  interMenu.classList.remove('catalog-open');
 }
