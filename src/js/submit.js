871b1c0d0668d97662461a124c1af51be8ba0a37
VitalinaShpir <grigorenko.vitalina@gmail.com>
2024-02-29 11:10:53 +0200

    fix mob menu

--- src/js/submit.js
+++ src/js/submit.js
@@ -1,8 +1,10 @@
 const openPopup = document.querySelector('.btn-submit');
+const openPopupMob = document.querySelector('.btn-submit-mob');
 const submitPopup = document.querySelector('.backdrop');
 const closePopup = document.querySelector('.popup-close-btn');
 
 openPopup.addEventListener('click', onOpenPopup);
+openPopupMob.addEventListener('click', onOpenPopup);
 closePopup.addEventListener('click', onClosePopup);
 
 function onOpenPopup() {
